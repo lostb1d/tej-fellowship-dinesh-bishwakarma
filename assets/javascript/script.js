0e2f654dfd730a022c71f5e3979f5af79e82dbc1
Dinesh Bishwakarma <55906126+lostb1d@users.noreply.github.com>
2022-04-17 18:37:51 +0545

    2

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+function showMessage(){
+    alert("Hello TEJ!");
+}
+
+function showAlert(){
+    alert("Hello there!");
+}
+
+
+var count = 0;
+
+function Inc(){
+    count++;
+    document.getElementById("count").innerHTML = count;
+}
+
+function Dec(){
+    count--;
+    document.getElementById("count").innerHTML = count;
+}
+
+
